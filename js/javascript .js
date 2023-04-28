37b5f3123ca86b53f60ab6f1c9c836f48837a7d6
mohamad-fawzy <mo.alborolusy@gmail.com>
2023-04-28 03:56:38 +0300

    end pro

---
+++ js/javascript .js
@@ -0,0 +1,34 @@
+let img = document.querySelector('.img')
+let bg = document.getElementById('cont')
+
+
+
+function phones(phone){
+
+    switch(phone)
+{
+    case 'golden':
+        img.src = "img/0.png";
+        bg.style.backgroundColor = '#A89191'
+        break
+    case 'blue' :
+        img.src = "img/1.png";
+        bg.style.backgroundColor = '#A89191'
+        break
+    case 'grey' :
+        img.src = "img/2.png";
+        bg.style.backgroundColor = '#A89191'
+        break
+    case 'ofwhet' :
+        img.src = "img/3.png";
+        bg.style.backgroundColor = '#A89191'
+        break
+    case 'red' :
+        img.src = "img/4.png";
+        bg.style.backgroundColor = '#A89191'
+        break
+
+
+}
+    
+}
