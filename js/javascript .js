15c6643aeefe91e238b22b7a2b881631d2e1f6c7
mohamad-fawzy <mo.alborolusy@gmail.com>
2024-01-27 15:03:06 +0200

    m

--- js/javascript .js
+++ js/javascript .js
@@ -9,26 +9,29 @@ function phones(phone){
 {
     case 'golden':
         img.src = "img/0.png";
-        bg.style.backgroundColor = '#A89191'
+        bg.style.backgroundColor = '#176B87'
         break
     case 'blue' :
         img.src = "img/1.png";
-        bg.style.backgroundColor = '#A89191'
+        bg.style.backgroundColor = '#A1ed'
         break
     case 'grey' :
         img.src = "img/2.png";
-        bg.style.backgroundColor = '#A89191'
+        bg.style.backgroundColor = '#748E63'
         break
     case 'ofwhet' :
         img.src = "img/3.png";
-        bg.style.backgroundColor = '#A89191'
+        bg.style.backgroundColor =  '#1D5D9B'
         break
     case 'red' :
         img.src = "img/4.png";
-        bg.style.backgroundColor = '#A89191'
+        bg.style.backgroundColor ='#E2C799'
         break
 
 
 }
     
 }
+
+
+
